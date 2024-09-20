08af9b650d16657750b8fed64ca17587ffc0f14d
PierluigiPapa <pierluigipapa9@gmail.com>
2024-09-20 17:42:14 +0200

    feat: lez #6 added routes LoginComponent and WelcomeComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular16';
 
-  saluti : string = "Benvenuti in PierFolio";
-
-  bollini : number = 150;
 }
