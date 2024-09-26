6fbf3b43d8dabc5fa038b97a2376bb9031eccc3c
PierluigiPapa <pierluigipapa9@gmail.com>
2024-09-26 16:57:06 +0200

    feat: lez #7 added code injection

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,21 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
   userID : string = "Pierluigi";
   password : string = "";
 
   autenticato : boolean = true;
-  consentito : boolean = false;
   errorMsg : string = "Spiacente, la userID e/o la password sono errati!";
   okMsg : string = "Accesso consentito";
 
-  constructor() {}
+  constructor(private route: Router) {}
 
     ngOnInit() : void{
     }
@@ -24,14 +24,12 @@ export class LoginComponent {
       console.log(this.userID)
 
       if (this.userID === "Pierluigi" && this.password === "Brindisi") {
+        this.route.navigate (['welcome']);
         this.autenticato = true;
-        this.consentito = true;
       }
       else {
         this.autenticato = false;
-        this.consentito = false;
       }
     }
-
-  }
+}
 
