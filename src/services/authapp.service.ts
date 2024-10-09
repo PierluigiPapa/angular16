c1d15c89f13f8e4578647c75b1f0e60fc1c97fb9
PierluigiPapa <pierluigipapa9@gmail.com>
2024-10-09 11:18:30 +0200

    feat: lez#19 added session storage, logout's stystem / fix: added disabled class on header.component.html

--- src/services/authapp.service.ts
+++ src/services/authapp.service.ts
@@ -8,6 +8,16 @@ export class AuthappService {
   constructor() { }
 
   autentica = (userID : string, password : string) : boolean => {
-    return (userID === 'Pierluigi' && password === 'Brindisi') ? true : false;
+    var retVal = (userID === 'Pierluigi' && password === 'Brindisi') ? true : false;
+
+    if (retVal) {
+      sessionStorage.setItem("Utente", userID);
+    }
+
+    return retVal
   }
+
+  loggedUser = () : string | null => (sessionStorage.getItem("Utente")) ? sessionStorage.getItem("Utente") : "";
+
+  isLogged = () : boolean => (sessionStorage.getItem("Utente")) ? true : false;
 }
