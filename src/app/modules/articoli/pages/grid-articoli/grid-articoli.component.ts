6a4c0e3110073e2a17c6eff83d5f4665c9c54f76
PierluigiPapa <pierluigipapa9@gmail.com>
2024-10-22 12:31:39 +0200

    feat: lez #25 added articoli card component / fix: fixed services

--- src/app/modules/articoli/pages/grid-articoli/grid-articoli.component.ts
+++ src/app/modules/articoli/pages/grid-articoli/grid-articoli.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { ArticoliService } from 'src/app/core/services/articoli.service';
+import { IArticoli } from 'src/app/shared/models/Articoli';
 
 @Component({
   selector: 'app-grid-articoli',
   templateUrl: './grid-articoli.component.html',
   styleUrls: ['./grid-articoli.component.css']
 })
-export class GridArticoliComponent {
+export class GridArticoliComponent  implements OnInit  {
+
+  articoli$ : IArticoli[] = [];
+
+  constructor(private articoliService: ArticoliService) { }
+
+  ngOnInit(): void {
+    this.articoli$ = this.articoliService.getArticoli();
+    console.log(this.articoli$);
+  }
 
 }
